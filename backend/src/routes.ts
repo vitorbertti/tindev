8e0502e0f0318b22058b1806189d32f4d3a3e5f1
Vitor Bertti <berttivitor@gmail.com>
2020-04-20 16:38:40 -0300

    Created dev model

---
+++ backend/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import DevController from './controllers/DevController';
+
+const routes = Router();
+
+routes.get('/devs', DevController.index);
+// routes.post('/devs', DevController.store);
+
+export default routes;
