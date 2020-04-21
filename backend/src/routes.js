2a3d13f7b9397257793bb626f85f833acabdc913
Vitor Bertti <berttivitor@gmail.com>
2020-04-21 20:02:34 -0300

    Created DislikeController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,6 @@ routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 
 routes.post('/devs/:devId/likes', LikeController.store);
-// routes.post('/devs/:devId/dislikes', DislikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
