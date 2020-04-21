f9013df35a27c9f33552821a6a346bd7c1b25526
Vitor Bertti <berttivitor@gmail.com>
2020-04-21 20:16:14 -0300

    Initialized frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = Router();
 
